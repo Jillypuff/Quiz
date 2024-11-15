ef25f62418345e3b4eb93f45cf1b7183b2c13005
Jesper Lindberg <JesperLindberg92@protonmail.com>
2024-11-15 11:15:48 +0100

    First commit

---
+++ src/Server.java
@@ -0,0 +1,15 @@
+import java.net.Socket;
+
+public class Server {
+
+    int port = 55554;
+    private Socket socket;
+
+    Server(){
+
+    }
+
+    public static void main(String[] args) {
+        new Server();
+    }
+}
