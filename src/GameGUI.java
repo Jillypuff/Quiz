3727d849797edb9203d394af6c84a052c5f97918
Jessica Sundin <jessundin@hotmail.com>
2024-11-18 21:49:28 +0100

    Class that handles user interface using Swing

---
+++ src/GameGUI.java
@@ -0,0 +1,26 @@
+import javax.swing.*;
+
+public class GameGUI {
+
+    Client client;
+    JFrame gameFrame;
+    JTextArea gameArea;
+    JTextField answerField;
+    JButton submitButton;
+
+    public GameGUI(Client client) {
+        this.client = client;
+    }
+
+    public void showLoginWindow() {
+
+    }
+
+    public void showMenu() {
+
+    }
+
+    private void startGame() {
+
+    }
+}
