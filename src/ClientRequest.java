2c5188f0756d0550a2a499a7a6fdb4158008b846
Jesper Lindberg <JesperLindberg92@protonmail.com>
2024-11-18 13:57:35 +0100

    start enum branch

---
+++ src/ClientRequest.java
@@ -0,0 +1,14 @@
+public enum ClientRequest {
+    START_GAME,
+    EXIT_GAME,
+    LEAVE_QUEUE,
+    // CHANGE_USERNAME,
+    // CHECK_HIGHSCORE,
+
+
+    ANSWER_ONE,
+    ANSWER_TWO,
+    ANSWER_THREE,
+    ANSWER_FOUR,
+    GIVE_UP,
+}
