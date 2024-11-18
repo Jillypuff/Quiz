b493db1c5fdbe088ab9f00904ecf3fb90e654c39
Jesper Lindberg <JesperLindberg92@protonmail.com>
2024-11-18 15:32:06 +0100

    worked on the protocols

---
+++ src/QuizActions.java
@@ -0,0 +1,7 @@
+public enum QuizActions {
+    SEND_QUESTION,
+    SEND_ANSWER,
+
+    SEND_SCORE,
+    SEND_FINAL_RESULT,
+}
---
+++ src/ServerResponse.java
@@ -0,0 +1,7 @@
+public enum ServerResponse {
+    CLIENT_CONNECTED,
+    CLIENT_DISCONNECTED,
+
+    QUEUE_JOINED,
+    GAME_JOINED,
+}
