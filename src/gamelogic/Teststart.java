417a72189115c27f3c4b0fb537370188e809fa44
Jesper Lindberg <JesperLindberg92@protonmail.com>
2024-11-22 10:12:45 +0100

    reading for merge

--- src/gamelogic/Teststart.java
+++
@@ -1,8 +0,0 @@
-package gamelogic;
-
-public class Teststart {
-     public static void main(String[] args) {
-         Activ_game a = new Activ_game();
-         Activ_game b = new Activ_game();
-     }
-}
