3f8776a9c71c14a7c5c7bcca18d786755e9b3455
Jessica Sundin <jessundin@hotmail.com>
2024-11-22 21:41:16 +0100

    updated waiting panel

--- src/GUI/WaitingPanel.java
+++ src/GUI/WaitingPanel.java
@@ -1,24 +1,30 @@
 package GUI;
 
 import javax.swing.*;
+import java.awt.*;
 import java.awt.event.ActionListener;
 
 public class WaitingPanel extends JPanel {
 
-    public JLabel welcomePrompt = new JLabel();
-    public JLabel queuedLabel = new JLabel("Waiting for other player to arrive");
+    public JLabel queuedLabel = new JLabel("Waiting for a player to join..");
     public JButton leaveQueueButton;
 
     public WaitingPanel() {
         setLayout(new BoxLayout(this, BoxLayout.Y_AXIS));
-
         add(Box.createVerticalStrut(50));
+
+        queuedLabel.setAlignmentX(CENTER_ALIGNMENT);
 
         leaveQueueButton = new JButton("Leave Queue");
 
-        add(welcomePrompt);
+        JPanel buttonPanel = new JPanel();
+        buttonPanel.setLayout(new FlowLayout(FlowLayout.CENTER));
+        buttonPanel.add(leaveQueueButton);
+
+
         add(queuedLabel);
-        add(leaveQueueButton);
+        add(Box.createVerticalStrut(40));
+        add(buttonPanel);
     }
 
     public void addActionListener(ActionListener listener){
