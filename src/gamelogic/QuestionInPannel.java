f5c439e9b87f3b8a7f4f2c2ee0749f5660b558b8
Joakim Skäppegård <joakim_skappegard@hotmail.com>
2024-11-25 00:35:01 +0100

    Lag till panel för att visa resultat.
    Små förändringar på ActivGame klassen och Question in panel Klassen. gjort seters and getters, dessa autogenererades, kan behöva ses över.

--- src/gamelogic/QuestionInPannel.java
+++ src/gamelogic/QuestionInPannel.java
@@ -4,5 +4,46 @@ public class QuestionInPannel {
     Question question;
     protected int recivedAwnser;
     protected boolean isCorrectPlayer1;
-    protected Kategori valdkategori;
+    protected boolean isCorrectPlayer2;
+    protected Category valdkategori;
+
+    public Question getQuestion() {
+        return question;
+    }
+
+    public void setQuestion(Question question) {
+        this.question = question;
+    }
+
+    public int getRecivedAwnser() {
+        return recivedAwnser;
+    }
+
+    public void setRecivedAwnser(int recivedAwnser) {
+        this.recivedAwnser = recivedAwnser;
+    }
+
+    public boolean isCorrectPlayer1() {
+        return isCorrectPlayer1;
+    }
+
+    public void setCorrectPlayer1(boolean correctPlayer1) {
+        isCorrectPlayer1 = correctPlayer1;
+    }
+
+    public boolean isCorrectPlayer2() {
+        return isCorrectPlayer2;
+    }
+
+    public void setCorrectPlayer2(boolean correctPlayer2) {
+        isCorrectPlayer2 = correctPlayer2;
+    }
+
+    public Category getValdkategori() {
+        return valdkategori;
+    }
+
+    public void setValdkategori(Category valdkategori) {
+        this.valdkategori = valdkategori;
+    }
 }
